d346e13645c2a7fa24c8cddd9550d32c6b631039
lululufr <91730933+lululufr@users.noreply.github.com>
2022-12-31 00:08:01 +0100

    Fin de la mise en place Ncurse- fonctionnel CLion

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
+#include <curses.h>                    /* ncurses.h includes stdio.h */
+#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include "monster.h"
 
-int main ()
+int main()
 {
-        card alexis =declare("alexis");
-        printcard(alexis);
- 
-        return 0;
-
-        //test
+    char mesg[]="Enter a string: ";         /* message to be appeared on the screen */
+    char str[80];
+    int row,col;                            /* Nan to store the number of rows and *
+                                                 * the number of colums of the screen */
+    initscr();                              /* start the curses mode */
+    getmaxyx(stdscr,row,col);               /* get the number of rows and columns */
+    mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
+    /* print the message at the center of the screen */
+    getstr(str);
+    mvprintw(LINES - 2, 0, "You Entered: %s", str);
+    getch();
+    endwin();
+    return 0;
 }
