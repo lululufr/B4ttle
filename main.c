a5dfc200cf6bbe740025c301f7dd47c15967ffb9
lululufr <91730933+lululufr@users.noreply.github.com>
2022-12-31 13:26:57 +0100

    Menu avec curses.h et menu.h

--- main.c
+++ main.c
@@ -7,8 +7,11 @@ int main() {
     char item[8];
     int ch, i = 0, width = 7;
     initscr(); // initialize Ncurses
+
     w = newwin( 10, 12, 1, 1 ); // create a new window
+
     box( w, 0, 0 ); // sets default borders for the window
+
 // now print all the menu items and highlight the first one
     for( i=0; i<5; i++ ) {
         if( i == 0 )
@@ -21,13 +24,17 @@ int main() {
     wrefresh( w ); // update the terminal screen
     i = 0;
     noecho(); // disable echoing of characters on the screen
+
     keypad( w, TRUE ); // enable keyboard input for the window.
+
     curs_set( 0 ); // hide the default screen cursor.
 // get the input
+
     while(( ch = wgetch(w)) != 'q'){
 // right pad with spaces to make the items appear with even width.
         sprintf(item, "%-7s",  list[i]);
         mvwprintw( w, i+1, 2, "%s", item );
+
 // use a variable to increment or decrement the value based on the input.
         switch( ch ) {
             case KEY_UP:
@@ -39,6 +46,7 @@ int main() {
                 i = ( i>4 ) ? 0 : i;
                 break;
         }
+
 // now highlight the next item in the list.
         wattron( w, A_STANDOUT );
         sprintf(item, "%-7s",  list[i]);
