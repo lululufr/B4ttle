18aeeadd2682505c0417002dc659f3258cdfc2b3
lululufr <91730933+lululufr@users.noreply.github.com>
2022-12-31 13:16:57 +0100

    Menu avec curses.h et menu.h

--- main.c
+++ main.c
@@ -1,20 +1,56 @@
-#include <curses.h>                    /* ncurses.h includes stdio.h */
-#include <string.h>
-#include <stdio.h>
+#include <curses.h>
+#include <menu.h>
+#include <stdlib.h>
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define CTRLD 	4
+
+char *choices[] = {
+        "Nouvelle Partie",
+        "Charger Partie",
+        "Options",
+        "Bibliotheque des monstres",
+        "Exit",
+};
 
 int main()
-{
-    char mesg[]="Enter a string: ";         /* message to be appeared on the screen */
-    char str[80];
-    int row,col;                            /* Nan to store the number of rows and *
-                                                 * the number of colums of the screen */
-    initscr();                              /* start the curses mode */
-    getmaxyx(stdscr,row,col);               /* get the number of rows and columns */
-    mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
-    /* print the message at the center of the screen */
-    getstr(str);
-    mvprintw(LINES - 2, 0, "You Entered: %s", str);
-    getch();
+{	ITEM **my_items;
+    int c;
+    MENU *my_menu;
+    int n_choices, i;
+    ITEM *cur_item;
+
+
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+
+    n_choices = ARRAY_SIZE(choices);
+    my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+
+    for(i = 0; i < n_choices; ++i)
+        my_items[i] = new_item(choices[i], choices[i]);
+    my_items[n_choices] = (ITEM *)NULL;
+
+    my_menu = new_menu((ITEM **)my_items);
+    mvprintw(LINES - 2, 0, "F1 to Exit");
+    post_menu(my_menu);
+    refresh();
+
+    while((c = getch()) != KEY_F(1))
+    {   switch(c)
+        {	case KEY_DOWN:
+                menu_driver(my_menu, REQ_DOWN_ITEM);
+                break;
+            case KEY_UP:
+                menu_driver(my_menu, REQ_UP_ITEM);
+                break;
+        }
+    }
+
+    free_item(my_items[0]);
+    free_item(my_items[1]);
+    free_menu(my_menu);
     endwin();
-    return 0;
 }
