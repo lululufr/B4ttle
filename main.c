e1da0711deba09cc54eedf6e213a9523a36e0dc1
lululufr <91730933+lululufr@users.noreply.github.com>
2022-12-31 13:22:46 +0100

    Menu avec curses.h et menu.h

--- main.c
+++ main.c
@@ -1,56 +1,50 @@
-#include <curses.h>
-#include <menu.h>
-#include <stdlib.h>
+#include<curses.h>
+#include<stdlib.h>
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
-#define CTRLD 	4
-
-char *choices[] = {
-        "Nouvelle Partie",
-        "Charger Partie",
-        "Options",
-        "Bibliotheque des monstres",
-        "Exit",
-};
-
-int main()
-{	ITEM **my_items;
-    int c;
-    MENU *my_menu;
-    int n_choices, i;
-    ITEM *cur_item;
-
-
-    initscr();
-    cbreak();
-    noecho();
-    keypad(stdscr, TRUE);
-
-    n_choices = ARRAY_SIZE(choices);
-    my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
-
-    for(i = 0; i < n_choices; ++i)
-        my_items[i] = new_item(choices[i], choices[i]);
-    my_items[n_choices] = (ITEM *)NULL;
-
-    my_menu = new_menu((ITEM **)my_items);
-    mvprintw(LINES - 2, 0, "F1 to Exit");
-    post_menu(my_menu);
-    refresh();
-
-    while((c = getch()) != KEY_F(1))
-    {   switch(c)
-        {	case KEY_DOWN:
-                menu_driver(my_menu, REQ_DOWN_ITEM);
-                break;
+int main() {
+    WINDOW *w;
+    char list[5][8] = { "One", "Two", "Three", "Four", "Five" };
+    char item[8];
+    int ch, i = 0, width = 7;
+    initscr(); // initialize Ncurses
+    w = newwin( 10, 12, 1, 1 ); // create a new window
+    box( w, 0, 0 ); // sets default borders for the window
+// now print all the menu items and highlight the first one
+    for( i=0; i<5; i++ ) {
+        if( i == 0 )
+            wattron( w, A_STANDOUT ); // highlights the first item.
+        else
+            wattroff( w, A_STANDOUT );
+        sprintf(item, "%-7s",  list[i]);
+        mvwprintw( w, i+1, 2, "%s", item );
+    }
+    wrefresh( w ); // update the terminal screen
+    i = 0;
+    noecho(); // disable echoing of characters on the screen
+    keypad( w, TRUE ); // enable keyboard input for the window.
+    curs_set( 0 ); // hide the default screen cursor.
+// get the input
+    while(( ch = wgetch(w)) != 'q'){
+// right pad with spaces to make the items appear with even width.
+        sprintf(item, "%-7s",  list[i]);
+        mvwprintw( w, i+1, 2, "%s", item );
+// use a variable to increment or decrement the value based on the input.
+        switch( ch ) {
             case KEY_UP:
-                menu_driver(my_menu, REQ_UP_ITEM);
+                i--;
+                i = ( i<0 ) ? 4 : i;
+                break;
+            case KEY_DOWN:
+                i++;
+                i = ( i>4 ) ? 0 : i;
                 break;
         }
+// now highlight the next item in the list.
+        wattron( w, A_STANDOUT );
+        sprintf(item, "%-7s",  list[i]);
+        mvwprintw( w, i+1, 2, "%s", item);
+        wattroff( w, A_STANDOUT );
     }
-
-    free_item(my_items[0]);
-    free_item(my_items[1]);
-    free_menu(my_menu);
+    delwin( w );
     endwin();
 }
